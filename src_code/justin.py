1a13ee7ba7b1c0c0541313fa80762a201a20516c
Coolguy <coolguy@cool.com>
2024-04-04 22:13:12 +0100

    Arrival

--- src_code/justin.py
+++ src_code/justin.py
@@ -1,5 +1,6 @@
 # This example requires the 'message_content' intent.
-
+import requests
+from bs4 import BeautifulSoup
 import discord
 import os
 
@@ -7,6 +8,21 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+
+def runescapeupdate():
+
+    response = requests.get('https://oldschool.runescape.com/l=2/')
+    html_page = response.text
+    soup = BeautifulSoup(html_page, 'html.parser')
+    url = soup.find(id="news-article-fig-1")
+
+    # Inside the webpage...
+    response2 = requests.get(url.get("href"))
+    soup2 = BeautifulSoup(response2.text, 'html.parser')
+    title = soup2.title
+
+    return url.get("href")
 
 
 @client.event
@@ -22,7 +38,11 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+    if 'runescape' in message.content.lower() and 'justin' in message.content.lower() and 'update' in message.content.lower():
+        await message.channel.send(runescapeupdate())
+
 
 token = os.environ['TOKEN']
 
 client.run(token)
+
